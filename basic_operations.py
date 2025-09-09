54db920f91a7e4c8e73c20b27ed386d1b13beacd
Peter-Kimani-Exothermic-cloud <p.kimani232@gmail.com>
2025-09-09 23:25:04 +0300

    simple interest task

---
+++ basic_operations.py
@@ -0,0 +1,10 @@
+number1 = 10
+number2 = 5
+
+addition = number1 + number2
+subtraction = number1 - number2
+multiplication = number1 * number2
+
+print(f"Addition of {number1} and {number2} is 15")
+print(f"Subtraction of {number1} and {number2} is 5")
+print(f"Multiplication of {number1} and {number2} is 50")
